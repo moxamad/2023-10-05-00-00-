b516bb4e84150f0a68ab1882ad516ed4ababfd03
moxamad <R8AGn5VhFSy7Rz@yandex.ru>
2024-08-10 00:07:33 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,34 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    valid_domains = [".com", ".ru", ".net"]
+
+    if not any(recipient.endswith(domain) for domain in valid_domains):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+        return
+
+    if not any(sender.endswith(domain) for domain in valid_domains):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+        return
+
+    if '@' not in recipient:
+        print("Адрес получателя некорректен, '@' отсутствует.")
+        return
+
+    if '@' not in sender:
+        print("Адрес отправителя некорректен, '@' отсутствует.")
+        return
+
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    if sender == "university.help@gmail.com":
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
